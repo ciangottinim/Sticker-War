fe34dd82ade74c5210127522548979126a6b5057
sk4nd <manuel.ciangottini@gmail.com>
2018-03-06 00:38:53 +0100

    initial commit

---
+++ Assets/ObjectClicked.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectClicked : MonoBehaviour
+{
+
+    bool isHighlighted = false;
+    Material originalMaterial;
+    Material redMaterial;
+    MeshRenderer meshRenderer;
+
+    GameObject baseObject;
+    string obj_name;
+    // Use this for initialization
+    void Start()
+    {
+
+        obj_name = this.gameObject.name + "Base";
+        baseObject = GameObject.Find(obj_name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+
+        Debug.Log("OMD " + obj_name);
+        Application.LoadLevel("NewGameScene");
+    }
+
+}
